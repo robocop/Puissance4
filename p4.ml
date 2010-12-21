88cf4ff70aedfccab4d9bc963fe26d2b73186a69
Quentin C <quentincormier@gmail.com>
2010-12-21 23:04:55 +0100

    ajout d'un petit minmax et d'une interface, premiere version fonctionelle (mais nulle)

--- p4.ml
+++ p4.ml
@@ -53,6 +53,9 @@ let cancel b =
   b.board.(x).(y) <- E;
   {b with whos_turn = !!(b.whos_turn); moves = List.tl b.moves }
 ;; 
+let make_cancel b = ignore (cancel b);;
+
+
 
  let print_board board = 
     let separator = "\n   +----+----+----+----+----+----+----+\n" in
@@ -88,20 +91,85 @@ let cancel b =
 ;;
 
  let is_win b =
-   let x, y = List.hd (b.moves) in
+   try
+     let x, y = List.hd (b.moves) in
      count b (x, y) (1,0) + count b (x, y) (-1,0) >= 3 
-  || count b (x, y) (0,1) + count b (x, y) (0,-1) >= 3 
-  || count b (x, y) (1,1) + count b (x, y) (-1,-1) >= 3 
-  || count b (x, y) (-1,1) + count b (x, y) (1,-1) >= 3 
+     || count b (x, y) (0,1) + count b (x, y) (0,-1) >= 3 
+     || count b (x, y) (1,1) + count b (x, y) (-1,-1) >= 3 
+     || count b (x, y) (-1,1) + count b (x, y) (1,-1) >= 3 
+   with _ -> false
 ;;
 
+
+ let get_valids_moves board = 
+   let l = ref [] in
+   for i = 0 to 6 do
+     if board.colones.(i) < 6 then l := i::!l;
+   done;
+   !l
+;;
+
+ let eval b = 
+   if is_win b then PInf else (N 0)
+ ;;
+ let max_score = 1;;
+
+ let print b = 
+   print_endline (string_of_score (eval b));
+   print_board b
+;;
+ let rec minmax b prof = 
+   let moves = get_valids_moves b in
+   let rec foreach_moves (best_score, best_move)= function
+     | [] ->  (best_score, best_move)
+     | m::list ->
+       let b = move b m in
+       let score = eval b in
+       if score = PInf then (make_cancel b; (score, m))
+       else
+	 let s, _ = if prof <= 0 then ((--)(eval b), m) else minmax b (prof-1) in
+	 make_cancel b;
+	 foreach_moves (if ((--) s) >>= best_score then ((--) s, m) else  (best_score, best_move)) list
+   in  
+   if moves = [] then (N 0, 0)
+   else
+      foreach_moves (MInf, 0) moves
+;; 
+
+
+
+ let _ = 
+   let couleur_joueur = R in
+   let b = {whos_turn = R; moves = []; colones = Array.make 7 0; board = Array.create_matrix 7 6 E} in
+   let rec loop b = 
+     print b;
+     match get_valids_moves b with
+     | [] -> print_endline "done";
+     | l ->
+       if couleur_joueur = b.whos_turn then 
+	 let coup = read_int () in
+	 if not (List.mem coup l) then loop b
+	 else
+	   loop (move b coup)
+       else
+	 let coup = snd (minmax b 6) in
+	 loop (move b coup)
+   in
+   loop b
+;;
+(*
 (* tests... *)
  let ib = {whos_turn = R; moves = []; colones = Array.make 7 0; board = Array.create_matrix 7 6 E};;
+ let b = move ib 1;;
+print_board b;;
+minmax b 3;;
  let b = move ib 3;;
  let b = move b 4;;
- let b = move b 1;;
- print_board b;
+ let b = move b 4;;
+get_valids_moves b;;
+ print_board ib;;
 
 count b (3,1) (1,0);;
 is_win b;;
 
+*)
