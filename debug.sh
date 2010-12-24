62dce82a1a9be19fafe13e7b1f1231c3248c0eb7
Quentin C <quentincormier@gmail.com>
2010-12-24 16:45:45 +0100

    .

--- debug.sh
+++ debug.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rm debug/*.ml
-sed -e 's/\(^open .*\)/(* \1 *)/' eval.ml > debug/eval.ml
 sed -e 's/\(^open .*\)/(* \1 *)/' extendn.ml > debug/extendn.ml
-sed -e 's/\(^open .*\)/(* \1 *)/' types.ml > debug/types.ml
+sed -e 's/\(^open .*\)/(* \1 *)/' board.ml > debug/board.ml
+sed -e 's/\(^open .*\)/(* \1 *)/' eval.ml > debug/eval.ml
 sed -e 's/\(^open .*\)/(* \1 *)/' p4.ml > debug/p4.ml
 cd debug/
 ocaml -init ../.ocamlinit
--- extendn.ml
+++ extendn.ml
@@ -5,7 +5,15 @@ let (--) = function
     | PInf -> MInf
     | MInf -> PInf
     | N n -> N (-n)
-
+let (++) a b = match a, b with
+  | (N a, N b) -> N (a+b)
+  | (PInf, N _) -> PInf
+  | (MInf, N _) -> MInf
+  | (N _, PInf) -> PInf
+  | (N _, MInf) -> MInf
+  | (MInf, MInf) -> MInf
+  | (PInf, PInf) -> PInf
+  | _ -> failwith "-oo + +oo is a non-sens"
 let (>>=) a b = match (a, b) with
     | PInf, MInf -> true
     | MInf, PInf -> false
